bc82956cd29e13fbb11eba651c296224a09c44fc
muxi114514 <1608055880@qq.com>
2026-04-09 23:38:45 +0800

    switch IaF compat from CE to original IaF (alexthe666)

--- src/main/java/com/mx/bettersurvival/BetterSurvival.java
+++ src/main/java/com/mx/bettersurvival/BetterSurvival.java
@@ -39,7 +39,7 @@ public class BetterSurvival {
                 isDefiledLoaded = net.minecraftforge.fml.ModList.get().isLoaded("defiledlands");
                 isJMixinLoaded = net.minecraftforge.fml.ModList.get().isLoaded("jmixin");
                 if (isIafLoaded)
-                        LOGGER.info("IaF CE detected — BS weapons will use IaF materials.");
+                        LOGGER.info("IaF detected — BS weapons will use IaF materials.");
                 if (isDefiledLoaded)
                         LOGGER.info("Defiled Lands detected — BS weapons will use Defiled materials.");
                 if (isJMixinLoaded)
